db86f2ca9d118605c54977c886637df5e05eb715
vmlr123 <107290237+vmlr123@users.noreply.github.com>
2026-03-10 23:29:00 -0400

    fixed bugs regarding validation, app now sends more info and error notifications, user is logged in automatically once successfully registered, and no duplicated products accepted per user

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const parseVErr = require("../utils/parseValidationErrs");
-const flash = require("connect-flash");
+const passport = require("passport");
 
 const registerShow = (req, res) => {
   res.render("register");
@@ -9,10 +9,15 @@ const registerShow = (req, res) => {
 const registerDo = async (req, res, next) => {
   if (req.body.password != req.body.password1) {
     req.flash("error", "The passwords entered do not match.");
-    return res.render("register", { errors: flash("errors") });
+    res.redirect("/sessions/register");
+    return res.render("register", { errors: req.flash("errors") });
   }
   try {
-    await User.create(req.body);
+    const user = await User.create(req.body);
+    req.login(user, (err) => {
+      if (err) return next(err);
+      return res.redirect("/");
+    });
   } catch (e) {
     if (e.constructor.name === "ValidationError") {
       parseVErr(e, req);
@@ -21,9 +26,9 @@ const registerDo = async (req, res, next) => {
     } else {
       return next(e);
     }
-    return res.render("register", { errors: flash("errors") });
+    res.redirect("/sessions/register");
+    return res.render("register", { errors: req.flash("errors") });
   }
-  res.redirect("/");
 };
 
 const logoff = (req, res) => {
