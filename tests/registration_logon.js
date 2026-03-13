41315b4e160dbcb78ee75a295bb361e801a23938
vmlr123 <107290237+vmlr123@users.noreply.github.com>
2026-03-13 09:39:13 -0400

    tests for logon and index page passed

--- tests/registration_logon.js
+++ tests/registration_logon.js
@@ -53,4 +53,42 @@ describe("tests for registration and logon", function () {
     newUser = await User.findOne({ email: this.user.email });
     expect(newUser).to.not.be.null;
   });
+
+  it("should log the user on", async () => {
+    const dataToPost = {
+      email: this.user.email,
+      password: this.password,
+      _csrf: this.csrfToken,
+    };
+    const { expect, request } = await get_chai();
+    const req = request
+      .execute(app)
+      .post("/sessions/logon")
+      .set("Cookie", this.csrfCookie)
+      .set("content-type", "application/x-www-form-urlencoded")
+      .redirects(0)
+      .send(dataToPost);
+    const res = await req;
+    expect(res).to.have.status(302);
+    expect(res.headers.location).to.equal("/");
+    const cookies = res.headers["set-cookie"];
+    this.sessionCookie = cookies.find((element) =>
+      element.startsWith("connect.sid"),
+    );
+    expect(this.sessionCookie).to.not.be.undefined;
+  });
+
+  it("should get the index page", async () => {
+    const { expect, request } = await get_chai();
+    const req = request
+      .execute(app)
+      .get("/")
+      .set("Cookie", this.csrfCookie)
+      .set("Cookie", this.sessionCookie)
+      .send();
+    const res = await req;
+    expect(res).to.have.status(200);
+    expect(res).to.have.property("text");
+    expect(res.text).to.include(this.user.name);
+  });
 });
